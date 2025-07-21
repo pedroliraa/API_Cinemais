81d6fd01a4afb3156b4a6ef0d20ae98f95d645b0
pedroliraa <pedrolirabandeira@gmail.com>
2025-07-20 23:48:48 -0300

    fix: padroniza mediaId em favorites para ser igual ao _id das mídias

--- src/services/favoritesService.js
+++ src/services/favoritesService.js
@@ -1,89 +1,83 @@
-import Favorite from '../models/Favorites.js';
-import { getMediasCatalogo } from './mediaService.js';
+import Favorite from '../models/Favorites.js'
+import { getMediasCatalogo } from './mediaService.js'
+import Media from '../models/Media.js'
+import mongoose from 'mongoose'
 
 export async function addFavoriteServ(userId, mediaId) {
-  await validaFavorite(userId, mediaId);
+    await validaFavorite(userId, mediaId)
 
-  //Checa se já existe o favorito para não duplicar
-  const exists = await Favorite.findOne({ userId, mediaId });
+    //Checa se já existe o favorito para não duplicar
+    const exists = await Favorite.findOne({ userId, mediaId })
 
-  if (!exists) {
-    await Favorite.create({ userId, mediaId });
-  }
+    if (!exists) {
+        await Favorite.create({ userId, mediaId })
+    }
 
-  return getUserFavoritesServ(userId);
+    return getUserFavoritesServ(userId)
 }
 
 export async function removeFavoriteServ(userId, mediaId) {
-  await validaFavorite(userId, mediaId, false);
+    await validaFavorite(userId, mediaId, false)
 
-  await Favorite.deleteOne({ userId, mediaId });
+    await Favorite.deleteOne({ userId, mediaId })
 
-  return getUserFavoritesServ(userId);
+    return getUserFavoritesServ(userId)
 }
 
 export async function getUserFavoritesServ(userId) {
-  if (!userId || typeof userId !== 'string' || userId.trim() === '') {
-    throw new Error(
-      "Por favor, o campo 'userId' é obrigatório e deve ser no formato texto"
-    );
-  }
+    if (!userId || typeof userId !== 'string' || userId.trim() === '') {
+        throw new Error(
+            "Por favor, o campo 'userId' é obrigatório e deve ser no formato texto"
+        );
+    }
 
-  const favorites = await Favorite.aggregate([
-    { $match: { userId } },
-    {
-      $lookup: {
-        from: 'media',
-        localField: 'mediaId',
-        foreignField: '_id',
-        as: 'mediaInfo',
-      },
-    },
-    { $unwind: '$mediaInfo' },
-    {
-      $project: {
-        _id: 1, // ID do favorito
-        title: '$mediaInfo.title',
-        description: '$mediaInfo.description',
-        type: '$mediaInfo.type',
-        releaseYear: '$mediaInfo.releaseYear',
-        genre: '$mediaInfo.genre',
-      },
-    },
-  ]);
+    const favorites = await Favorite.aggregate([
+        { $match: { userId } },
+        {
+            $lookup: {
+                from: Media.collection.name,         
+                localField: 'mediaId',
+                foreignField: '_id',
+                as: 'mediaInfo',
+            },
+        },
+        { $unwind: '$mediaInfo' },
 
-  return favorites;
+        { $replaceRoot: { newRoot: '$mediaInfo' } },
+    ]);
+
+    return favorites
 }
 
 async function validaFavorite(
-  userId,
-  mediaId = undefined,
-  checarExistencia = true
+    userId,
+    mediaId = undefined,
+    checarExistencia = true
 ) {
-  if (!userId || typeof userId !== 'string' || userId.trim() === '') {
-    throw new Error(
-      "Por favor, o campo 'userId' é obrigatório e deve ser no formato texto"
-    );
-  }
-
-  if (mediaId !== undefined) {
-    if (!mediaId || typeof mediaId !== 'string' || mediaId.trim() === '') {
-      throw new Error(
-        "Por favor, o campo 'mediaId' é obrigatório e deve ser no formato texto"
-      );
+    if (!userId || typeof userId !== 'string' || userId.trim() === '') {
+        throw new Error(
+            "Por favor, o campo 'userId' é obrigatório e deve ser no formato texto"
+        );
     }
 
-    if (checarExistencia) {
-      // Aqui consultamos o catálogo de mídias no mongo
+    if (mediaId !== undefined) {
+        if (!mediaId || typeof mediaId !== 'string' || mediaId.trim() === '') {
+            throw new Error(
+                "Por favor, o campo 'mediaId' é obrigatório e deve ser no formato texto"
+            );
+        }
 
-      const catalogo = await getMediasCatalogo();
-      const mediaExiste = catalogo.some(
-        (media) => media._id.toString() === mediaId
-      );
+        if (checarExistencia) {
+            // Aqui consultamos o catálogo de mídias no mongo
 
-      if (!mediaExiste) {
-        throw new Error(`A media com id '${mediaId}' não existe no catálogo`);
-      }
+            const catalogo = await getMediasCatalogo()
+            const mediaExiste = catalogo.some(
+                (media) => media._id.toString() === mediaId
+            );
+
+            if (!mediaExiste) {
+                throw new Error(`A media com id '${mediaId}' não existe no catálogo`)
+            }
+        }
     }
-  }
 }
--- src/services/mediaService.js
+++ src/services/mediaService.js
@@ -1,22 +1,22 @@
-import MediaModel from '../models/Media.js';
+import MediaModel from '../models/Media.js'
 
 //cria uma nova mídia
 export async function createNewMedia(mediaData) {
   validaMedias(mediaData);
 
-  const newMedia = await MediaModel.create(mediaData);
+  const newMedia = await MediaModel.create(mediaData)
 
-  return newMedia;
+  return newMedia
 }
 
 //retorna todo o catálogo
 export async function getMediasCatalogo() {
-  return MediaModel.find();
+  return MediaModel.find()
 }
 
 //busca mídia pelo ID
 export async function getMediabyID(id) {
-  return MediaModel.findById(id);
+  return MediaModel.findById(id)
 }
 
 //valida os campos da mídia
@@ -26,7 +26,7 @@ export function validaMedias(data) {
     typeof data.title !== 'string' ||
     data.title.trim() === ''
   ) {
-    throw new Error("Por favor, o campo 'Título' não pode estar vazio");
+    throw new Error("Por favor, o campo 'Título' não pode estar vazio")
   }
 
   if (
@@ -34,7 +34,7 @@ export function validaMedias(data) {
     typeof data.description !== 'string' ||
     data.description.trim() === ''
   ) {
-    throw new Error("Por favor, o campo 'Descrição' não pode estar vazio");
+    throw new Error("Por favor, o campo 'Descrição' não pode estar vazio")
   }
 
   if (
@@ -42,11 +42,11 @@ export function validaMedias(data) {
     typeof data.genre !== 'string' ||
     data.genre.trim() === ''
   ) {
-    throw new Error("Por favor, o campo 'Gênero' não pode estar vazio");
+    throw new Error("Por favor, o campo 'Gênero' não pode estar vazio")
   }
 
   if (!data.type || typeof data.type !== 'string' || data.type.trim() === '') {
-    throw new Error("Por favor, o campo 'Tipo' não pode estar vazio");
+    throw new Error("Por favor, o campo 'Tipo' não pode estar vazio")
   }
 
   if (data.releaseYear === undefined || data.releaseYear === null) {
@@ -56,10 +56,10 @@ export function validaMedias(data) {
   }
 
   if (data.type !== 'movie' && data.type !== 'series') {
-    throw new Error("Por favor, o campo 'Tipo' deverá ser 'movie' ou 'series'");
+    throw new Error("Por favor, o campo 'Tipo' deverá ser 'movie' ou 'series'")
   }
 
-  const anoAtual = new Date().getFullYear();
+  const anoAtual = new Date().getFullYear()
 
   if (
     typeof data.releaseYear !== 'number' ||
